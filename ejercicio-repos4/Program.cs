97baa747a0087d5cb6d271d38e6b2af47bd4d2fb
Flores V�squez Carlos Gianfranco <FV23003@ues.edu.sv>
2024-09-10 17:13:56 -0600

    ingreso de Div

--- ejercicio-repos4/Program.cs
+++ ejercicio-repos4/Program.cs
@@ -11,15 +11,17 @@ namespace ejercicio_repos4
         static void Main(string[] args)
         {
             Double Num1, Num2;
-            Double Suma = 0, Multi = 0;
+            Double Suma = 0, Multi = 0,Div=0;
             Console.WriteLine("Ingrese el numero 1: ");
             Num1 = Double.Parse(Console.ReadLine());
             Console.WriteLine("Ingrese el numero 2: ");
             Num2 = Double.Parse(Console.ReadLine());
             Suma = Num1 + Num2;
             Multi = Num1 * Num2;
+            Div = Num1 / Num2;
             Console.WriteLine("El resultado de la suma es de:{0} ", Suma);
             Console.WriteLine("El resultado de la Multiplicacion es: {1}", Multi);
+            Console.WriteLine("El resultado de la Multiplicacion es: {2}", Div);
             Console.ReadKey();
         }
     }
